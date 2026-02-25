188a7f5269b10cc7da06b398e13e04e40a4f925c
Morgan <ycshinichi@163.com>
2026-02-25 09:35:13 +0800

    Creating Components and passing props and binded prop

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container justify-content-center align-items-center">
+    <div class="align-items-center">
+      <h2 class="mb-3">{{ msg }}</h2>
+      <ContactUs name="John" phone="123-456-7890" ownerName="vue" />
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import ContactUs from './components/Contact.vue';
+
+const msg = 'Hello Vue'
+</script>
---
+++ src/components/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+        <div class="bg-info rounded p-2 m-2  d-inline-block">
+                <h3>Contact Us</h3>
+                <p>Name : {{name }}</p>
+                <p>Email : {{ email }}</p>
+                <p>Phone : {{ phone }}</p>
+                <p class="float-end small" v-if="ownerName != ''">
+                 This contact info belongs to{{ ownerName }}</p>
+        </div>
+</template>
+
+<script setup>
+        import { defineProps } from 'vue';
+        const email = 'vue@vue.org';
+        const props = defineProps(["name", "phone", "ownerName"]);
+</script>
