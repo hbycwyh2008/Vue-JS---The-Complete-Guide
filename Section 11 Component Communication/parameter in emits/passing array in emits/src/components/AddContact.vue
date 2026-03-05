90850d97b8533583a911dd7987cb4aff07396986
Morgan <ycshinichi@163.com>
2026-03-05 09:26:05 +0800

    Add Section 12 Provide and Inject - new contact component & update Section 11 parameter in emits

---
+++ Section 11 Component Communication/parameter in emits/passing array in emits/src/components/AddContact.vue
@@ -0,0 +1,48 @@
+<template>
+        <div class="container border rounded p-2">
+                <div class="text-center text-white">Add Contact</div>
+                <form @submit.prevent="addContact">
+                        <div class="row">
+                                <div class="col-4">
+                                        <input type="text" class="form-control" 
+                                               v-model="contact.name" placeholder="Enter Name"/>
+                                </div>
+
+                                 <div class="col-4">
+                                        <input type="text" class="form-control" 
+                                               v-model="contact.phone" placeholder="Enter Phone"/>
+                                </div>
+
+                                 <div class="col-4">
+                                        <input type="text" class="form-control" 
+                                               v-model="contact.email" placeholder="Enter Email"/>
+                                </div>
+                                <div class="col-6" offset-3 p-2>
+                                        <button type="submit" class="btn btn-secondary w-100">Submit</button>
+                                </div>
+                        
+                              
+
+                        </div>
+                        
+                </form>
+
+        </div>
+
+
+</template>
+
+<script setup>
+import { reactive } from "vue";
+const contact = reactive({
+  name: "",
+  phone: "",
+  email: "",
+});
+function addContact() {
+  console.log(contact);
+  contact.name = "";
+  contact.phone = "";
+  contact.email = "";
+}
+</script>
