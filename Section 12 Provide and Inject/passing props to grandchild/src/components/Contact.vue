d19cac324894b22c2ae42dbcfefd8291b0e8643b
Morgan <ycshinichi@163.com>
2026-03-09 13:34:54 +0800

    passing props to grandchild components

---
+++ Section 12 Provide and Inject/passing props to grandchild/src/components/Contact.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="m-2">
+    <div class="bg-info rounded p-2">
+      <div class="row">
+        <div class="col-6">
+          <p>Name : {{ name }}</p>
+          <p>Email : {{ email }}</p>
+          <p>Phone : {{ phone }}</p>
+        </div>
+        <div class="col-3">
+          <button @click="toggleFavorite" :class="isFavorite ? 'btn btn-warning' : 'btn btn-success'">
+                {{ isFavorite ? 'Remove From' : 'Add To' }} Favorite
+          </button>
+        </div>
+        <div class="col-3">
+          <LuckyNumber :maxNumber="maxLuckyNumber"/>
+        </div>
+      </div>
+    </div>
+    <span class="small text-end mb-0 mt-1 float-end" v-if="ownerName != ''">
+      This contact info belongs to {{ ownerName }}</span>
+  </div>
+</template>
+
+<script setup>
+import LuckyNumber from './LuckyNumber.vue';
+const props = defineProps({
+  name: { type: String, required: true },
+  phone: String,
+  isFavorite: Boolean,
+  ownerName: String,
+  maxLuckyNumber: Number,
+  email: {
+    type: String,
+    required: true,
+    default: "N/A",
+  },
+});
+
+const emit = defineEmits(['update-favorite']);
+function toggleFavorite() {
+  emit('update-favorite',[props.isFavorite,props.name]);
+}
+</script>
