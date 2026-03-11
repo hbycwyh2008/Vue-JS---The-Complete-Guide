9a5f23898fd9096899f22027096e50d457a3891e
Morgan <ycshinichi@163.com>
2026-03-11 12:56:47 +0800

    provide and inject

---
+++ Section 12 Provide and Inject/provide and inject/src/components/LuckyNumber.vue
@@ -0,0 +1,21 @@
+<template>
+        <div class="float-end">
+                <button class="btn btn-outline-danger" 
+                @click="generateLuckyNumber()">Generate Lucky Number</button>
+                <span v-if="luckyNumber===0" > Generate lucky number <br/>
+                        Range: 1- {{props.maxNumber-1}}
+                </span>
+                <span v-else> Your lucky number is: {{luckyNumber}} <br/>
+                        Generate another lucky number
+                </span>      
+        </div>
+
+</template>
+<script setup>
+import {ref,inject} from 'vue'
+let luckyNumber = ref(0);
+const maxNumber = inject("maxLuckyNumber");
+function generateLuckyNumber() {
+        luckyNumber.value = Math.floor(Math.random() * props.maxNumber);
+}
+</script>
