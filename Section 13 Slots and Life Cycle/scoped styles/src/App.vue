d74e55a86b224bed7d95a91ca9890d8ee6272fcc
Morgan <ycshinichi@163.com>
2026-03-17 09:20:06 +0800

    scoped styles

---
+++ Section 13 Slots and Life Cycle/scoped styles/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <luckyNumberParentComponent>
+    <template v-slot:default>
+      <p class="pt-2">We have two versions for picking lucky numbers!</p>
+      <hr/>
+    </template>
+    
+    <template v-slot:moreInfo>
+      <p>Click the button to toggle between those two versions.</p>
+    </template>
+    
+    <template v-slot:learnSlots>
+       <button @click="showMessage">what weill we learn</button>
+      <h4 class="text-success">{{ message }}</h4>
+
+    </template>
+   
+    
+  </luckyNumberParentComponent>
+</template>
+<script setup>
+import LuckyNumberParentComponent from './components/LuckyNumberParentComponent.vue';
+import {ref} from 'vue';
+const message = ref('');
+function showMessage() {
+  message.value = 'We will learn how to use slots.';
+}
+</script>
