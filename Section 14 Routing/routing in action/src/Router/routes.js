1c05900581d6057dd1791d65fb8be68ad6a38786
Morgan <ycshinichi@163.com>
2026-03-19 13:52:32 +0800

    rounting in action

---
+++ Section 14 Routing/routing in action/src/Router/routes.js
@@ -0,0 +1,11 @@
+import HomePage from "../components/Home/HomePage.vue";
+import Contact from "../components/Home/Contact.vue";
+import { createRouter,createWebHistory } from "vue-router";
+const router = createRouter({
+        history:createWebHistory(),
+        routes:[
+                { path: '/', component: HomePage },
+                {path:'/contact', component: Contact},
+        ]
+});
+export default router;
