a015603d788c0c3c6b8cd766b60c97e17c9c3649
Morgan <ycshinichi@163.com>
2026-03-23 09:18:43 +0800

    add header

---
+++ Section 14 Routing/add header/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Products
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Product List</a></li>
+            <li><a class="dropdown-item" href="#">Product Details</a></li> 
+          </ul>
+        </li>
+        <li class="nav-item">
+          
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <a aria-disabled = "true" class="nav-link">Login</a>
+      </form>
+    </div>
+  </div>
+</nav>
+      <router-view></router-view>
+    </div>
+</template>
+
